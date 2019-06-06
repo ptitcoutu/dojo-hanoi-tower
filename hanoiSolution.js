3d3c2fac0d75938a022f9a2935bdb5c67d56af15
ptitcoutu <vincent.couturier@gmail.com>
2019-06-06 13:20:24 +0200

    2 disks

--- hanoiSolution.js
+++ hanoiSolution.js
@@ -1,7 +1,9 @@
 
 
 function moveDisks(numberOfDiskToMove, startRodIndex, targetRodIndex, intermediaryRodIndex) {
-   document.moveDisk(0, 2)
+  document.moveDisk(0, 1)
+  document.moveDisk(0, 2)
+  document.moveDisk(1, 2)
 }
 
 exports.moveDisks = moveDisks
