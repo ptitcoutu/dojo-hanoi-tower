c9728c8a7f5a62b379e7d653635ef6174449b08a
ptitcoutu <vincent.couturier@gmail.com>
2019-06-06 13:24:16 +0200

    1 file modified

--- index.js
+++ index.js
@@ -2,11 +2,11 @@
 import './style.css';
 import { moveDisksRecursively } from './recursiveSolution.js';
 import { moveDisks } from './hanoiSolution.js';
-import { diskCount } from './hanoiPlay.js'
+import { getDisksCount } from './hanoiPlay.js'
 import { check } from './hanoiSolutionCheck.js'
 
 function solve() {
-  moveDisks(diskCount, 0, 2, 1);
+  moveDisks(getDisksCount(), 0, 2, 1);
   check();
 }
 
