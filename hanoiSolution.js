96a592a22b8398ca9201da428f5b5395a338f1c4
ptitcoutu <vincent.couturier@gmail.com>
2019-06-06 13:48:40 +0200

    Manage 3 disks

--- hanoiSolution.js
+++ hanoiSolution.js
@@ -1,13 +1,17 @@
 
 
 function moveDisks(numberOfDiskToMove, startRodIndex, targetRodIndex, intermediaryRodIndex) {
-  if (numberOfDiskToMove == 1) {
-    document.moveDisk(0, 2)
-  } else if (numberOfDiskToMove == 2) {
-    document.moveDisk(0, 1)
-    document.moveDisk(0, 2)
-    document.moveDisk(1, 2)
-  }
+ if (numberOfDiskToMove == 1) {
+   document.moveDisk(startRodIndex, targetRodIndex)
+ } else if (numberOfDiskToMove == 2) {
+   document.moveDisk(startRodIndex, intermediaryRodIndex)
+   document.moveDisk(startRodIndex, targetRodIndex)
+   document.moveDisk(intermediaryRodIndex, targetRodIndex)
+ } else if (numberOfDiskToMove == 3) {
+   moveDisks(2,0,1,2)
+   document.moveDisk(startRodIndex, targetRodIndex)
+   moveDisks(2,1,2,0)
+ }
 }
 
 exports.moveDisks = moveDisks
