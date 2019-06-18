888416cd2fb35cf96d8901f81acd8f3a21a20081
ptitcoutu <vincent.couturier@gmail.com>
2019-06-18 13:29:23 +0200

    last dojo

--- hanoiSolution.js
+++ hanoiSolution.js
@@ -1,17 +1,18 @@
 
-
+/**
+ * moveDisks(3, 0, 2, 1)
+ */
 function moveDisks(numberOfDiskToMove, startRodIndex, targetRodIndex, intermediaryRodIndex) {
- if (numberOfDiskToMove == 1) {
-   document.moveDisk(startRodIndex, targetRodIndex)
- } else if (numberOfDiskToMove == 2) {
-   document.moveDisk(startRodIndex, intermediaryRodIndex)
-   document.moveDisk(startRodIndex, targetRodIndex)
-   document.moveDisk(intermediaryRodIndex, targetRodIndex)
- } else if (numberOfDiskToMove == 3) {
-   moveDisks(2,0,1,2)
-   document.moveDisk(startRodIndex, targetRodIndex)
-   moveDisks(2,1,2,0)
- }
+    if (numberOfDiskToMove == 1) {
+      document.moveDisk(startRodIndex, targetRodIndex);
+    } else if (numberOfDiskToMove == 2) {
+      document.moveDisk(startRodIndex, intermediaryRodIndex);
+      document.moveDisk(startRodIndex, targetRodIndex);
+      document.moveDisk(intermediaryRodIndex, targetRodIndex);
+    } else if (numberOfDiskToMove == 3) {
+      moveDisks(2, 0, 1, 2);
+    }
+    
 }
 
 exports.moveDisks = moveDisks
